06e61ca40594f928197a460bab8b2ccfce5584ac
simonhighfield <highfieldsimon@gmail.com>
2024-05-20 16:48:37 +0100

    Created a context for App.js's GigInfoVisible state, to pass into search and saved to make the gig info visible or not

--- screens/SearchScreen.jsx
+++ screens/SearchScreen.jsx
@@ -1,11 +1,22 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Pressable } from "react-native";
 import { GigDetails } from "../components/GigDetails";
+import { useContext } from "react";
+import GigInfoVisibleContext from "../contexts/GigInfoVisibleContext";
 
 export function SearchScreen() {
+    const { gigInfoVisible, setGigInfoVisible } = useContext(GigInfoVisibleContext)
+    
+    function toggleGigInfoVisible() {
+        setGigInfoVisible(!gigInfoVisible)
+    }
+
     return (
         <View>
             <Text>Search Screen</Text>
-            <GigDetails />
+            <Pressable onPress={toggleGigInfoVisible}>
+                <Text>hit me</Text>
+            </Pressable>
+            {gigInfoVisible && <GigDetails />}
         </View>
     )
 }
