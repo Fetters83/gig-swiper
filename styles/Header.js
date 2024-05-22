d733e077fad4d1b24b1dd654d29b5d59177f85cc
DevTomUK <tomghdev@gmail.com>
2024-05-22 12:27:37 +0100

    Changes log out button styling

--- styles/Header.js
+++ styles/Header.js
@@ -1,20 +1,20 @@
-import { Button } from "react-native";
+import { Button, Text } from "react-native";
 import { signOut } from "firebase/auth";
 import { auth } from "../config/Config";
 import { Search } from "../components/Search";
 import { useState } from "react";
 
-  function handleLogout(){
-      return signOut(auth).then((data)=>{console.log(data , "was an error")})
-      .catch((err)=>{
-          console.log(err, "EEEEEEEEEEEEEEEE")
-      })
-  }
-
-
+function handleLogout() {
+  return signOut(auth)
+    .then((data) => {
+      console.log(data, "was an error");
+    })
+    .catch((err) => {
+      console.log(err, "EEEEEEEEEEEEEEEE");
+    });
+}
 
 export const headerStyle = {
-  
   headerStyle: {
     backgroundColor: "#f4511e",
   },
@@ -26,8 +26,8 @@ export const headerStyle = {
     <Button
       onPress={handleLogout}
       title="Log Out"
-      color="#fff"
-      style={marginHorizontal=50}
-    />
-  )
+      color="#666"
+      style={(marginHorizontal = 50)}
+    ></Button>
+  ),
 };
