903cc74b6ca07d1cfe782074a5ddd208729814df
dasartini <adriansartinid@gmail.com>
2024-05-22 16:42:17 +0100

    adding params to axios getAllEvents method, building fuctionallity in Search.jsx

--- api.js
+++ api.js
@@ -1,17 +1,39 @@
 import axios from 'axios'
 
 
-export function getAllEvents(){
+// export function getAllEvents(latitudeValue, longitudeValue, radiusValue){
 
-return axios.get('https://www.skiddle.com/api/v1/events/search/?api_key=53e664e9d779d1a9ba1d2a248bb01777/')
-.then(({data})=>{return data.results})
+// return axios.get('https://www.skiddle.com/api/v1/events/search/?api_key=53e664e9d779d1a9ba1d2a248bb01777/&',
+//     {params :{
+// latitude: latitudeValue,
+// longitude: longitudeValue,
+// radius: radiusValue
+//     }}
+// )
+// .then(({data})=>{
+   
+//     return data.results})
 
+// }
+
+export function getAllEvents(latitude, longitude, radius){
+    console.log(latitude , longitude, radius)
+    return axios.get(`https://www.skiddle.com/api/v1/events/search/?api_key=53e664e9d779d1a9ba1d2a248bb01777/&`, {
+        params: {
+            latitude: latitude,
+            longitude: longitude,
+            radius: radius
+        }}
+    )
+    .then(({data})=>{
+       
+        return data.results})
 }
 
 // Maps
-export function fetchLocation(locationSearch) {
+export function fetchLatitudeAndLongitude(locationSearch) {
     return axios.get(`https://nominatim.openstreetmap.org/search?q=gb%20${locationSearch}&format=json&addressdetails=1&limit=1&polygon_svg=1`)
-.then(({data})=>{
-    console.log(data.lat, data.lon);
-    return data})
+    .then(({data})=>{
+        return {latitude: data[0].lat, longitude: data[0].lon}
+    })
 }
