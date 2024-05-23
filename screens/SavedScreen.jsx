3008fee3fc6e068485cfd643bb7ec52debe24c28
Rory Fletcher <roryfletcher2002@gmail.com>
2024-05-23 13:47:07 +0100

    likedgigcard

--- screens/SavedScreen.jsx
+++ screens/SavedScreen.jsx
@@ -7,10 +7,6 @@ export function SavedScreen({toggleGigInfoVisible}) {
         <ScrollView>
             <Text>Saved Screen</Text>
             <LikedGig toggleGigInfoVisible={toggleGigInfoVisible}/>
-            <LikedGig toggleGigInfoVisible={toggleGigInfoVisible}/>
-            <LikedGig toggleGigInfoVisible={toggleGigInfoVisible}/>
-            <LikedGig toggleGigInfoVisible={toggleGigInfoVisible}/>
-            <LikedGig toggleGigInfoVisible={toggleGigInfoVisible}/>
         </ScrollView>
     )
 }
