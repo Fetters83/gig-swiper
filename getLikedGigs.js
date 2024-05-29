db18e9369f0bc1214dff85d1c613d50e29ce6c56
DevTomUK <tomghdev@gmail.com>
2024-05-29 13:55:30 +0100

    fixed unhandled promise error

--- getLikedGigs.js
+++ getLikedGigs.js
@@ -1,17 +1,12 @@
 import { collection, getDocs } from "firebase/firestore";
 import db from "./firebaseConfig";
 
-import { useContext } from "react";
-import { LikedGigContext } from "./contexts/LikedGigContext";
-
 export default async function getLikedGigs(user) {
   const querySnapshot = await getDocs(collection(db, "users"));
   let newLikes = [];
   querySnapshot.forEach((doc) => {
-    if (doc.id === user.email) {
-      console.log(doc.id, user.email, "info in async");
+    if ((doc.id === user.email) & user) {
       newLikes = doc.data().likedgigs;
-      console.log(newLikes, "NEW LIKES IN GET REQ");
     }
   });
   return newLikes;
