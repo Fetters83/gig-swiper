98db435b00716a6bf6e074ba5ecf628432823dfa
simonhighfield <highfieldsimon@gmail.com>
2024-05-30 09:52:47 +0100

    Refactored GigInfoModal.jsx

--- components/GIgInfoModal.jsx
+++ components/GIgInfoModal.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, Pressable, Image, ScrollView, Button, Linking } from "react-native";
+import { Text, View, Pressable, Image, ScrollView, Button, Linking } from "react-native";
 import { styles } from "./GigInfoModal.styles";
 
 export function GigInfoModal(props) {
