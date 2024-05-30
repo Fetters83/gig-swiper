6fee876478c96254d9925631d0cd164b37d0ebb5
simonhighfield <highfieldsimon@gmail.com>
2024-05-30 09:42:06 +0100

    Extracted into GigInfoModal.style

--- components/GIgInfoModal.jsx
+++ components/GIgInfoModal.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import { StyleSheet, Text, View, Pressable, Image, ScrollView, Button, Linking } from "react-native";
-
+import { styles } from "./GigInfoModal.styles";
 
 export function GigInfoModal(props) {
-
   const { toggleGigInfoVisible, currentGig, stackNumber } = props
-
   const [showVenueDetails, setShowVenueDetails] = useState(false)
   const [showGenres, setShowGenres] = useState(false)
+  
   function toggleVenueDetails() {
     setShowVenueDetails(!showVenueDetails)
   }
@@ -16,11 +15,8 @@ export function GigInfoModal(props) {
     setShowGenres(!showGenres)
   }
 
-
   return (
-
     <View style={styles.container}>
-
       <Image style={styles.topImage} source={{ uri: currentGig.xlargeimageurl }} />
       <Pressable onPress={toggleGigInfoVisible} style={styles.closeButton}>
         <Image style={styles.closeButtonImage} source={require('../assets/close.png')} />
@@ -66,94 +62,3 @@ export function GigInfoModal(props) {
 
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    width: '100%',
-    height: '100%'
-  },
-  header: {
-    fontSize: 20,
-  },
-  text: {
-    fontSize: 14,
-  },
-  modalCenter: {
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#00000080',
-  },
-  modalOuter: {
-    margin: 20,
-    backgroundColor: '#dde',
-    borderRadius: 10,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    height: 500,
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    }
-  },
-  topImage: {
-    width: '100%',
-    height: 200,
-    objectFit: 'cover',
-  },
-  topImageContainer: {
-    width: '100%',
-  },
-  marginVert: {
-    marginVertical: 10,
-  },
-  padding: {
-    padding: 10,
-  },
-  fullWidth: {
-    width: '100%',
-  },
-  descriptionWidth:{
- width: "60%"
-  },
-  largeText: {
-    fontSize: 24,
-  },
-  mediumText: {
-    fontSize: 18,
-  },
-  smallText: {
-    fontSize: 16,
-  },
-  row: {
-    flexDirection: 'row',
-  },
-  spaceBetween: {
-    justifyContent: 'space-between',
-    // alignItems: 'space-between'
-  },
-  closeButton: {
-    position: 'absolute',
-    right: 10,
-    top: 20,
-  },
-  closeButtonImage: {
-    width: 50,
-    height: 50,
-  },
-  darkerBackground: {
-    backgroundColor: '#ccc',
-  },
-  dateBox: {
-    alignItems: 'center',
-  },
-  border: {
-    borderWidth: 2,
-  }
-
-});
