172c2196bad0c6da0567c4c22a361c29f31cb3f5
DevTomUK <tomghdev@gmail.com>
2024-05-30 10:11:16 +0100

    fixes radius value issue

--- App.js
+++ App.js
@@ -121,7 +121,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "silver",
+    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
   },
