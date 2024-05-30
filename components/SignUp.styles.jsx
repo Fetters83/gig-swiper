961bad6e551acd2569fffa60ac3a62432ad1df60
simonhighfield <highfieldsimon@gmail.com>
2024-05-30 10:12:36 +0100

    Refactored SignUp.jsx

---
+++ components/SignUp.styles.jsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    text:{
+      marginTop: 20,
+      textAlign: "center"
+    },
+    logOut: {
+      marginTop: 20
+    },
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: '#fff',
+      padding: 20,
+    },
+    input: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginBottom: 12,
+      paddingHorizontal: 8,
+    },
+});
--- components/SingUp.jsx
+++ components/SingUp.jsx
@@ -1,13 +1,13 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { useContext, useState } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
-import { StyleSheet } from 'react-native';
 import { auth } from '../config/Config';
 import { LoadingContext } from '../contexts/LoadingContext';
 import Loader from './Loader';
+import { styles } from './SignUp.styles'
 
 
-function SignUp({navigation}) {
+export default function SignUp({navigation}) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [validCredentials, setValidCredentials] = useState(true)
@@ -16,20 +16,21 @@ function SignUp({navigation}) {
   function handleSignUp() {
     setLoading(true)
     if (email && password) {
-      return createUserWithEmailAndPassword(auth, email, password).then((data)=>{
-        setLoading(false)
-        console.log("logged in")})
-        .catch((err) => {
-          console.log(err)
-         setValidCredentials(false)
-
+      return createUserWithEmailAndPassword(auth, email, password)
+        .then((data)=>{
+          setLoading(false)
+        })
+        .catch((error) => {
+          console.log("handleSignUp error caught:", error)
+          setValidCredentials(false)
         })
     }
-
   }
+
   function handleLink(){
     navigation.navigate("login")
   }
+
   function handleDevAccess() {
     return signInWithEmailAndPassword(auth, 'fiveguys@dev.com', 'Password')
     .then((data) => {
@@ -55,43 +56,15 @@ function SignUp({navigation}) {
       />
       <Loader/>
       <Button title="Sign Up Mate" onPress={handleSignUp} />
+
       <View style={styles.logOut}>
+        <Button onPress={handleLink} id="login" title={"Already have an account?"}></Button>
+        {!validCredentials && <Text style={styles.text}> Check your credentials</Text>}
+      </View>
 
-     <Button onPress={handleLink} id="login" title={"Already have an account?"}></Button>
-     {!validCredentials && <Text style={styles.text}> Check your credentials</Text>}
-
-     </View>
-   <View>
-    <Button title="Dev Access" onPress={handleDevAccess} />
-   </View>
+      <View>
+        <Button title="Dev Access" onPress={handleDevAccess} />
+      </View>
     </View>
-  
   );
 }
-
-const styles = StyleSheet.create({
-  text:{
-    marginTop: 20,
-textAlign: "center"
-
-  },
-  logOut: {
- marginTop: 20
-  
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-    padding: 20,
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 12,
-    paddingHorizontal: 8,
-  },
-});
-
-export default SignUp;
