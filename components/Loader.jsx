b0df835076071c7c0826bda646c082cef00f14b5
simonhighfield <highfieldsimon@gmail.com>
2024-05-30 10:25:18 +0100

    Refactored Loader

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -2,31 +2,20 @@ import React, { useContext } from "react";
 import {StyleSheet, View, ActivityIndicator, Text} from 'react-native';
 import { LoadingContext } from "../contexts/LoadingContext";
 
-function Loader(){
-const {loading, setLoading} = useContext(LoadingContext)
+export default function Loader() {
+  const {loading, setLoading} = useContext(LoadingContext)
 
-
-return(
-  <View style={styles.container}>
-<ActivityIndicator color="#f4511e" animating={loading}/>
-
-  </View>
-)
-
+  return(
+    <View style={styles.container}>
+    <ActivityIndicator color="#f4511e" animating={loading}/>
+    </View>
+  )
 }
 
-
 const styles= StyleSheet.create({
-
   container:{
-
-flexDirection:'row',
-justifyContent: 'space-around',
-padding: 10,
-
+    flexDirection:'row',
+    justifyContent: 'space-around',
+    padding: 10,
   }
-
-
 })
-
-export default Loader
